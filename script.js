d24cd186f2f94a3d2c7e0853f27ea21f1a1c86a6
guest-2000 <user.scp.d2000@gmail.com>
2026-09-11 04:02:10 +0300

    Add functionality to change the size of the grid

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-const container = document.querySelector(".container");
-for (let i = 0; i < 16 * 16; i++) {
-    const square = document.createElement("div");
-    square.classList.toggle("square");
-    square.addEventListener("mouseover", () => square.style.backgroundColor = "black");
-    container.appendChild(square);
+function createGrid(size) {
+    const container = document.querySelector(".container");
+    document.querySelectorAll(".container div").forEach(element => {
+        container.removeChild(element);
+    });
+    for (let i = 0; i < size * size; i++) {
+        const square = document.createElement("div");
+        square.style.backgroundColor = "aqua";
+        square.style.width = `${960 / size}px`;
+        square.style.height = `${960 / size}px`;
+        square.addEventListener("mouseover", () => square.style.backgroundColor = "black");
+        container.appendChild(square);
+    }
 }
+
+const btnGridSize = document.getElementById("grid-size-button");
+btnGridSize.addEventListener("click", function() {
+    const newSize = +prompt("Enter the new grid size", "");
+    if (newSize <= 100) {
+        createGrid(newSize);
+    } else {
+        alert("Warning: Size limit exceeded!");
+    }
+});
+createGrid(16);
